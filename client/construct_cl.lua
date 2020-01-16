ddc62c21f14adb32f101043e62c6e531dd372d65
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2020-01-15 21:37:22 -0700

    Cleanup/lint code
    Also replaced some logic with modulo's `%`

--- client/construct_cl.lua
+++ client/construct_cl.lua
@@ -10,50 +10,44 @@ local shadows = {}
 
 function OnKeyPress(key)
 	if key == "Y" then
-        consactivated=not consactivated
-        if (consactivated==false) then
-           CallRemoteEvent("RemoveShadow")
+        consactivated = not consactivated
+        if (consactivated == false) then
+            CallRemoteEvent("RemoveShadow")
         end
     end
     if key == "E" then
-        if (consactivated==true) then
-           remove_obj=not remove_obj
-           if remove_obj then
-            CallRemoteEvent("RemoveShadow")
-           end
+        if (consactivated == true) then
+            remove_obj = not remove_obj
+            if remove_obj then
+        		CallRemoteEvent("RemoveShadow")
+            end
         end
     end
     if key == "Mouse Wheel Up" then
-        if (curstruct+1==numb_of_objs+1) then
-            curstruct=1
-        else
-            curstruct=curstruct+1
-        end    
+		curstruct = curstruct + 1
+        curstruct = ((curstruct - 1) % numb_of_objs) + 1
     end
     if key == "Mouse Wheel Down" then
-		if (curstruct-1==0) then
-            curstruct=numb_of_objs
-        else
-            curstruct=curstruct-1
-        end    
+		curstruct = curstruct + 1
+		curstruct = (curstruct % numb_of_objs) + 1
     end
     if key == "R" then
-        if (currotyaw+90>180 ) then
-           currotyaw=-90
+        if (currotyaw + 90 > 180) then
+            currotyaw = -90
         else
-            currotyaw=currotyaw+90
+            currotyaw = currotyaw + 90
         end
     end
     if key == "Left Mouse Button" then
         if consactivated then
             if (remove_obj==false) then
-            CallRemoteEvent("Createcons")
+            	CallRemoteEvent("Createcons")
             else
                 local ScreenX, ScreenY = GetScreenSize()
-                 SetMouseLocation(ScreenX/2, ScreenY/2)
+                SetMouseLocation(ScreenX/2, ScreenY/2)
                 local entityType, entityId = GetMouseHitEntity()
                 if (entityId~=0) then
-                CallRemoteEvent("Removeobj",entityId)
+                	CallRemoteEvent("Removeobj",entityId)
                 end
             end
         end
@@ -68,70 +62,69 @@ local lastang = nil
 local lastcons = nil
 
 local lastconsactivated = nil
+
 function tickhook(DeltaSeconds)
-   if consactivated then
-    local ScreenX, ScreenY = GetScreenSize()
-    SetMouseLocation(ScreenX/2, ScreenY/2)
-    if remove_obj==false then
-    lastconsactivated=true
-    local x,y,z = GetMouseHitLocation()
-      if (x~=lasthitposx or y~=lasthitposy or z~=lasthitposz or lastang~=currotyaw or lastconsactivated~=consactivated or lastcons~=curstruct) then
-          lasthitposx=x
-          lasthitposy=y
-          lasthitposz=z
-          lastang=currotyaw
-          lastcons=curstruct
-          lastconsactivated=true
-          if (x~=0) then 
-            local entityType, entityId = GetMouseHitEntity()
-            local pitch,yaw,roll = GetCameraRotation()
-              CallRemoteEvent("UpdateCons",curstruct,currotyaw,x,y,z,entityId,yaw)
-          else
-            AddPlayerChat("Please look at valid locations")
-          end
-      end
-    end
-    else
+    if consactivated then
+		local ScreenX, ScreenY = GetScreenSize()
+		SetMouseLocation(ScreenX/2, ScreenY/2)
+		if remove_obj == false then
+		lastconsactivated = true
+		local x,y,z = GetMouseHitLocation()
+			if (x ~= lasthitposx or y ~= lasthitposy or z ~= lasthitposz or lastang ~= currotyaw or lastconsactivated ~= consactivated or lastcons ~= curstruct) then
+				lasthitposx = x
+				lasthitposy = y
+				lasthitposz = z
+				lastang = currotyaw
+				lastcons = curstruct
+				lastconsactivated = true
+				if (x ~= 0) then
+					local entityType, entityId = GetMouseHitEntity()
+					local pitch,yaw,roll = GetCameraRotation()
+			    	CallRemoteEvent("UpdateCons", curstruct, currotyaw, x, y, z, entityId, yaw)
+				else
+					AddPlayerChat("Please look at valid locations")
+				end
+		    end
+		end
+	else
         lastconsactivated=false
-   end
+    end
 end
 AddEvent("OnGameTick", tickhook)
 
-AddRemoteEvent("Createdobj",function(objid,collision)
+AddRemoteEvent("Createdobj", function(objid, collision)
     local delay = 50
-    if (GetPing()~=0) then
-        delay=GetPing()*6
+    if (GetPing() ~= 0) then
+        delay = GetPing() * 6
     end
     Delay(delay,function()
-    GetObjectActor(objid):SetActorEnableCollision(collision)
-    SetObjectCastShadow(objid, collision)
-    EnableObjectHitEvents(objid , collision)
+	    GetObjectActor(objid):SetActorEnableCollision(collision)
+	    SetObjectCastShadow(objid, collision)
+	    EnableObjectHitEvents(objid , collision)
     end)
 end)
 
-AddRemoteEvent("numberof_objects",function(number)
-    numb_of_objs=number
-
+AddRemoteEvent("numberof_objects", function(number)
+    numb_of_objs = number
 end)
 
 function render_cons()
     if consactivated then
-    DrawText(5, 400, "Press Y to toggle construction")
-    DrawText(5, 425, "Press E to toggle remove constructions")
-    DrawText(5, 450, "Press R to rotate your construction")
-    DrawText(5, 475, "Use the mouse wheel to change your object")
-    DrawText(5, 500, "Use the left click to place your object")
-    if remove_obj then
-        local entityType, entityId = GetMouseHitEntity()
-            if (entityId~=0) then
+	    DrawText(5, 400, "Press Y to toggle construction")
+	    DrawText(5, 425, "Press E to toggle remove constructions")
+	    DrawText(5, 450, "Press R to rotate your construction")
+	    DrawText(5, 475, "Use the mouse wheel to change your object")
+	    DrawText(5, 500, "Use the left click to place your object")
+	    if remove_obj then
+	        local entityType, entityId = GetMouseHitEntity()
+            if (entityId ~= 0) then
                 local x, y, z = GetObjectLocation(entityId)
                 local bResult, ScreenX, ScreenY = WorldToScreen(x, y, z)
                 if bResult then
-                    DrawText(ScreenX-40, ScreenY, "Left Click to remove")
+                    DrawText(ScreenX - 40, ScreenY, "Left Click to remove")
                 end
             end
-    end
+    	end
     end
 end
-
 AddEvent("OnRenderHUD", render_cons)
--- server/construct.lua
+++ server/construct.lua
@@ -5,17 +5,16 @@ local objs = {}
 objs[1] = 240 --Don't change this line
 objs[2] = 387 --Don't change this line
 objs[3] = 387 --Don't change this line
---objs[4] = 1003	
+--objs[4] = 1003
 --objs[index] = object id -- https://dev.playonset.com/wiki/Objects
 
 
 local admins_remove = {}
 
-admins_remove["76561197972837186"] = true -- me
+--admins_remove["76561197972837186"] = true -- me
 --admins_remove["steamid"] = true
 
 local pitchstairs = 45
-
 
 local constructed = {}
 
@@ -24,229 +23,229 @@ local shadows = {}
 function rshadow(ply)
     if (shadows[ply]) then
         DestroyObject(shadows[ply].mapobjid)
-        table.remove(shadows,ply)
+        table.remove(shadows, ply)
     end
  end
- 
+
  AddRemoteEvent("RemoveShadow",rshadow)
 
 function constructshadow(ply,conid,angle,x,y,z,hitentity,camrot)
     local anglex = 0
     local size = 1
-    if (conid==1) then
+    if (conid == 1) then
         size = 2
-        if (angle==180) then
-            angle=0
-            x=x-300
-        elseif (angle==-90) then
-           angle=90
-           y=y-300
-        elseif (angle==90) then
-            y=y-300
-        elseif (angle==0) then
-            x=x-300
+        if (angle == 180) then
+            angle = 0
+            x = x - 300
+        elseif (angle == -90) then
+           angle = 90
+           y = y - 300
+        elseif (angle == 90) then
+            y = y - 300
+        elseif (angle == 0) then
+            x = x - 300
         end
     end
-    if (conid==2) then
+    if (conid == 2) then
         anglex = 45
         size = 0.25
-        z=z+175
+        z = z + 175
     end
-    if (conid==3) then
+    if (conid == 3) then
         size = 0.25
-        z=z+25
+        z = z + 25
     end
     local identifier = CreateObject(objs[conid], x, y, z , anglex, angle, 0, size, size, size)
     if (identifier~=false) then
-    shadows[ply] = {}
-    shadows[ply].objid = conid
-    shadows[ply].mapobjid = identifier
-    for k,v in ipairs(GetAllPlayers()) do 
-       CallRemoteEvent(v,"Createdobj",identifier,false)
-    end
+        shadows[ply] = {}
+        shadows[ply].objid = conid
+        shadows[ply].mapobjid = identifier
+        for k,v in ipairs(GetAllPlayers()) do
+           CallRemoteEvent(v,"Createdobj",identifier,false)
+        end
     else
         print("Error at CreateObject Construction mod")
     end
 end
 
 function upcons(ply,conid,angle,x,y,z,hitentity,camrot)
-   if (shadows[ply]) then
-      if (shadows[ply].objid==conid) then
-        --AddPlayerChat(ply,"X : " .. x .. " Y : " .. y .. " Z : " .. z .. " Angle : " .. angle)
-        if (conid==1) then
-            local befangle = angle
-            if (angle==180) then
-                angle=0
-            elseif (angle==-90) then
-               angle=90
-            end
-            local nofound = true
-           for k,v in ipairs(constructed) do -- IDK if you can read the code below
-            if (v.mapobjid==hitentity and v.objid==1) then
-                local rotx , roty , rotz = GetObjectRotation(hitentity)
-                local ox, oy, oz = GetObjectLocation(hitentity)
-                nofound=false
-                local valtoadd = 0
-                if (roty==90 or roty==-90) then
-                if (y-oy>300) then
-                    if (angle==90) then
-                   valtoadd=600
-                   else
-                    valtoadd=600
+    if (shadows[ply]) then
+        if (shadows[ply].objid==conid) then
+            --AddPlayerChat(ply,"X : " .. x .. " Y : " .. y .. " Z : " .. z .. " Angle : " .. angle)
+            if (conid == 1) then
+                local befangle = angle
+                if (angle == 180) then
+                    angle = 0
+                elseif (angle == -90) then
+                   angle = 90
+                end
+                local nofound = true
+                for k,v in ipairs(constructed) do -- IDK if you can read the code below
+                    if (v.mapobjid == hitentity and v.objid == 1) then
+                        local rotx , roty , rotz = GetObjectRotation(hitentity)
+                        local ox, oy, oz = GetObjectLocation(hitentity)
+                        nofound = false
+                        local valtoadd = 0
+                        if (roty == 90 or roty == -90) then
+                            if (y - oy > 300) then
+                                if (angle == 90) then
+                                    valtoadd = 600
+                                else
+                                    valtoadd = 600
+                                end
+                            else
+                                if (angle == 90) then
+                                    valtoadd = -600
+                                else
+                                    valtoadd = 0
+                                end
+                            end
+                        else
+                            if (x - ox > 300) then
+                                if (angle == 0) then
+                                    valtoadd = 600
+                                else
+                                     valtoadd = 600
+                                end
+                            else
+                                if (angle == 0) then
+                                    valtoadd = -600
+                                else
+                                    valtoadd = 0
+                                end
+                            end
+                        end
+                        x = ox
+                        y = oy
+                        z = oz
+                        if (befangle == 180 and roty == 90) then
+                            y = oy + valtoadd
+                            x = ox - 600
+                        elseif (befangle == -90 and roty == 0) then
+                            x = ox + valtoadd
+                            y = oy - 600
+                        elseif (roty == 0) then
+                            x = ox + valtoadd
+                        elseif (roty == 90) then
+                            y = oy + valtoadd
+                        end
                     end
-                else
-                    if (angle==90) then
-                        valtoadd=-600
-                        else
-                         valtoadd=0
-                         end
                 end
-                else
-                    if (x-ox>300) then
-                        if (angle==0) then
-                            valtoadd=600
-                            else
-                             valtoadd=600
-                             end
-                     else
-                        if (angle==0) then
-                            valtoadd=-600
-                            else
-                             valtoadd=0
-                             end
-                     end
-                end
-                x=ox
-                y=oy
-                z=oz
-                if (befangle==180 and roty==90) then
-                    y=oy+valtoadd
-                    x=ox-600
-                elseif (befangle==-90 and roty==0) then
-                    x=ox+valtoadd
-                    y=oy-600
-                elseif (roty==0) then
-                    x=ox+valtoadd
-                elseif (roty==90) then
-                      y=oy+valtoadd
+                if (nofound == true) then
+                    if (angle == 0) then
+                       x = x - 300
+                    elseif (angle == 90) then
+                        y = y - 300
+                    end
                 end
             end
-           end
-           if (nofound==true) then
-           if (angle==0) then
-            x=x-300
-         elseif (angle==90) then
-           y=y-300
-         end
-        end
-        end
-        if (conid==2) then
-            z=z+175
-            for k,v in ipairs(constructed) do
-               if (v.mapobjid==hitentity and v.objid==2) then
-                  local rotx , roty , rotz = GetObjectRotation(hitentity)
-                  local ox, oy, oz = GetObjectLocation(hitentity)
-                  local valtoadd = 0
-                  if (z-oz>175) then
-                      z=oz+350
-                      valtoadd=350
-                  else
-                      z=oz-350
-                      valtoadd=-350
-                  end
-                  x=ox
-                  y=oy
-                  angle=roty
-                  if (roty==0) then
-                    x=ox+valtoadd
-                 elseif (roty==90) then
-                   y=oy+valtoadd
-                 elseif (roty==180) then
-                  x=ox+valtoadd*-1
-                 elseif (roty==-90) then
-                  y=oy+valtoadd*-1
-                 end
-               end
-            end
-        end
-        if (conid==3) then
-            z=z+25
-            for k,v in ipairs(constructed) do
-                if (v.mapobjid==hitentity and v.objid==3) then
-                    local rotx , roty , rotz = GetObjectRotation(hitentity)
-                    local ox, oy, oz = GetObjectLocation(hitentity)
-                    local valtoadd = 0
-                    if (x-ox>0 and y-oy>0) then
-                       if (x-ox>y-oy) then
-                        x=ox+500
-                        z=oz
-                        y=oy
-                       else
-                        x=ox
-                        z=oz
-                        y=oy+500
-                       end
-                    elseif (x-ox<0 and y-oy<0) then
-                        if (x-ox<y-oy) then
-                            x=ox-500
-                            z=oz
-                            y=oy
-                           else
-                            x=ox
-                            z=oz
-                            y=oy-500
-                        end
-                    elseif (x-ox<0) then
-                        if ((x-ox)*-1>y-oy) then
-                            x=ox-500
-                            z=oz
-                            y=oy
+            if (conid == 2) then
+                z = z + 175
+                for k,v in ipairs(constructed) do
+                    if (v.mapobjid == hitentity and v.objid == 2) then
+                        local rotx , roty , rotz = GetObjectRotation(hitentity)
+                        local ox, oy, oz = GetObjectLocation(hitentity)
+                        local valtoadd = 0
+                        if (z - oz > 175) then
+                          z = oz + 350
+                          valtoadd = 350
                         else
-                            x=ox
-                            z=oz
-                            y=oy+500
+                          z = oz - 350
+                          valtoadd = -350
                         end
-                    elseif (y-oy<0) then
-                        if ((y-oy)*-1>x-ox) then
-                            x=ox
-                            z=oz
-                            y=oy-500
-                        else
-                            x=ox+500
-                            z=oz
-                            y=oy
+                        x = ox
+                        y = oy
+                        angle = roty
+                        if (roty == 0) then
+                            x = ox + valtoadd
+                        elseif (roty == 90) then
+                            y = oy + valtoadd
+                        elseif (roty == 180) then
+                            x = ox + valtoadd * -1
+                        elseif (roty == -90) then
+                            y = oy + valtoadd * -1
                         end
                     end
                 end
             end
+            if (conid == 3) then
+                z = z + 25
+                for k, v in ipairs(constructed) do
+                    if (v.mapobjid == hitentity and v.objid == 3) then
+                        local rotx , roty , rotz = GetObjectRotation(hitentity)
+                        local ox, oy, oz = GetObjectLocation(hitentity)
+                        local valtoadd = 0
+                        if (x - ox > 0 and y - oy > 0) then
+                            if (x - ox > y - oy) then
+                                x = ox+500
+                                z = oz
+                                y = oy
+                            else
+                                x = ox
+                                z = oz
+                                y = oy + 500
+                            end
+                        elseif (x - ox < 0 and y - oy < 0) then
+                            if (x - ox < y - oy) then
+                                x = ox - 500
+                                z = oz
+                                y = oy
+                            else
+                                x = ox
+                                z = oz
+                                y = oy - 500
+                            end
+                        elseif (x - ox < 0) then
+                            if ((x - ox) * -1 > y - oy) then
+                                x = ox - 500
+                                z = oz
+                                y = oy
+                            else
+                                x = ox
+                                z = oz
+                                y = oy + 500
+                            end
+                        elseif (y - oy < 0) then
+                            if ((y - oy) * -1 > x - ox) then
+                                x = ox
+                                z = oz
+                                y = oy - 500
+                            else
+                                x = ox + 500
+                                z = oz
+                                y = oy
+                            end
+                        end
+                    end
+                end
+            end
+            local px, py, pz = GetPlayerLocation(ply)
+            local dist = GetDistance3D(px, py, pz, x, y, z)
+            if (dist<10000) then
+                SetObjectLocation(shadows[ply].mapobjid, x, y, z)
+                local anglex = 0
+                if (conid == 2) then
+                    anglex = 45
+                end
+                SetObjectRotation(shadows[ply].mapobjid, anglex, angle, 0)
+            else
+                AddPlayerChat(ply,"Too far from you")
+            end
+        else
+            DestroyObject(shadows[ply].mapobjid)
+            table.remove(shadows,ply)
+            constructshadow(ply,conid,angle,x,y,z,hitentity,camrot)
         end
-        local px, py, pz = GetPlayerLocation(ply)
-        local dist = GetDistance3D(px, py, pz, x, y, z)
-        if (dist<10000) then
-        SetObjectLocation(shadows[ply].mapobjid, x, y, z)
-        local anglex = 0
-        if (conid==2) then
-            anglex = 45
-        end
-        SetObjectRotation(shadows[ply].mapobjid, anglex, angle, 0)
     else
-        AddPlayerChat(ply,"Too far from you")
+        constructshadow(ply,conid,angle,x,y,z,hitentity,camrot)
     end
-    else
-        DestroyObject(shadows[ply].mapobjid)
-        table.remove(shadows,ply)
-        constructshadow(ply,conid,angle,x,y,z,hitentity,camrot)
-      end
-    else
-        constructshadow(ply,conid,angle,x,y,z,hitentity,camrot)
-   end
 end
 
 AddRemoteEvent("UpdateCons",upcons)
 
 function OnPlayerQuit(ply)
     rshadow(ply)
-    if (remove_objs_cons==true) then
+    if (remove_objs_cons == true) then
         local steamid = tostring(GetPlayerSteamId(ply))
 
         local index = 1 -- DjCtavia#3870
@@ -264,29 +263,29 @@ end
 AddEvent("OnPlayerQuit", OnPlayerQuit)
 
 function Createobj(ply)
-   if (shadows[ply]) then
-    local objtocreate = shadows[ply].mapobjid
-      local tbltoinsert = {}
-      tbltoinsert.mapobjid = shadows[ply].mapobjid
-      tbltoinsert.objid = shadows[ply].objid
-      tbltoinsert.owner = tostring(GetPlayerSteamId(ply))
-      table.insert(constructed,tbltoinsert)
-      for k,v in ipairs(GetAllPlayers()) do 
-       CallRemoteEvent(v,"Createdobj",shadows[ply].mapobjid,true)
-     end
-      table.remove(shadows,ply)
-   end
-end
-
-AddRemoteEvent("Createcons",Createobj)
-
-function coll_desync_workaround()
-    for kobj,vobj in ipairs(shadows) do
-    local identifier = vobj.mapobjid
-    for k,v in ipairs(GetAllPlayers()) do 
-       CallRemoteEvent(v,"Createdobj",identifier,false)
+    if (shadows[ply]) then
+        local objtocreate = shadows[ply].mapobjid
+        local tbltoinsert = {}
+        tbltoinsert.mapobjid = shadows[ply].mapobjid
+        tbltoinsert.objid = shadows[ply].objid
+        tbltoinsert.owner = tostring(GetPlayerSteamId(ply))
+        table.insert(constructed,tbltoinsert)
+        for k,v in ipairs(GetAllPlayers()) do
+            CallRemoteEvent(v, "Createdobj", shadows[ply].mapobjid, true)
+        end
+        table.remove(shadows, ply)
     end
 end
+
+AddRemoteEvent("Createcons", Createobj)
+
+function coll_desync_workaround()
+    for kobj, vobj in ipairs(shadows) do
+        local identifier = vobj.mapobjid
+        for k,v in ipairs(GetAllPlayers()) do
+           CallRemoteEvent(v, "Createdobj", identifier, false)
+        end
+    end
 end
 
 
@@ -295,21 +294,21 @@ AddEvent("OnPackageStart", function()
 end)
 
 function OnPlayerSpawn(ply)
-    CallRemoteEvent(ply,"numberof_objects",#objs)
+    CallRemoteEvent(ply, "numberof_objects", #objs)
 end
 AddEvent("OnPlayerSpawn", OnPlayerSpawn)
 
 function Removeobj(ply,hitentity)
     local steamid = tostring(GetPlayerSteamId(ply))
     for i,v in ipairs(constructed) do
-       if (hitentity==v.mapobjid) then
-          if (v.owner == steamid or admins_remove[steamid]) then
-             DestroyObject(hitentity)
-             table.remove(constructed,i)
-          else
-            AddPlayerChat(ply,"You can't remove this object")
-          end
-       end
+        if (hitentity==v.mapobjid) then
+            if (v.owner == steamid or admins_remove[steamid]) then
+                DestroyObject(hitentity)
+                table.remove(constructed,i)
+            else
+                AddPlayerChat(ply,"You can't remove this object")
+            end
+        end
     end
 end
-AddRemoteEvent("Removeobj",Removeobj)
+AddRemoteEvent("Removeobj", Removeobj)
