1948176ccca9da22720b6757b96797094034b3ad
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2020-01-17 22:01:54 -0700

    Adjust some of the offset

--- shared.lua
+++ shared.lua
@@ -8,13 +8,13 @@ CONSTRUCTION_OBJECTS[1] = {
     ID = 240,
     BaseRotation = {0, 0, 180},
     Scale = {2, 2, 2},
-    Offset = {300, 0, 500}
+    Offset = {-300, 0, 400}
 }
 CONSTRUCTION_OBJECTS[2] = {
     ID = 387,
     BaseRotation = {45, 0, 0},
     Scale = {0.25, 0.25, 0.25},
-    Offset = {0, 0, 250}
+    Offset = {0, 0, 200}
 }
 CONSTRUCTION_OBJECTS[3] = {
     ID = 387,
