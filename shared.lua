3a01c246e23e7cb631b4dd30df776a0b03434110
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2020-01-18 01:05:57 -0700

    Adjust staircase offset

--- shared.lua
+++ shared.lua
@@ -18,7 +18,7 @@ CONSTRUCTION_OBJECTS[2] = {
     BaseRotation = {45, 0, 0},
     Scale = {0.3, 0.3, 0.3},
     RelativeOffset = {0, 0, 200},
-    SelfOffset = {300, 300, 0},
+    SelfOffset = {300, 300, 200},
     GlobalOffset = {0, 0, 0}
 }
 CONSTRUCTION_OBJECTS[3] = {
