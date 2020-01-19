5b0bedb6471f7ea9e64a42fa71e72b10a83dfe91
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2020-01-18 17:36:07 -0700

    Remove bad wall, remove swaplayout on walls

--- shared.lua
+++ shared.lua
@@ -6,14 +6,6 @@ CONSTRUCTION_ID_PROPERTY_NAME = GetPackageName() .. "::construction_id"
 CONSTRUCTION_OBJECTS = {}
 
 CONSTRUCTION_OBJECTS[1] = {
-    ID = 240,
-    BaseRotation = {0, 0, 180},
-    Scale = {2, 2, 2},
-    RelativeOffset = {-300, 0, 400},
-    SelfOffset = {0, 0, 0},
-    GlobalOffset = {300, -300, 0}
-}
-CONSTRUCTION_OBJECTS[2] = {
     ID = 387,
     BaseRotation = {33.7, 0, 0},
     Scale = {0.3606, 0.3, 0.3},
@@ -21,7 +13,7 @@ CONSTRUCTION_OBJECTS[2] = {
     SelfOffset = {300, 300, 400},
     GlobalOffset = {0, 0, 0}
 }
-CONSTRUCTION_OBJECTS[3] = {
+CONSTRUCTION_OBJECTS[2] = {
     ID = 387,
     BaseRotation = {0, 0, 0},
     Scale = {0.3, 0.3, 0.3},
@@ -33,22 +25,20 @@ CONSTRUCTION_OBJECTS[3] = {
     SwapLayout = true,
     GlobalOffset = {0, 0, 10}
 }
-CONSTRUCTION_OBJECTS[4] = {
+CONSTRUCTION_OBJECTS[3] = {
     ID = 851,
     BaseRotation = {0, 0, 0},
     Scale = {0.75, 1, 0.8},
     RelativeOffset = {-300, 0, 0},
     SelfOffset = {0, 0, 0},
-    SwapLayout = true,
     GlobalOffset = {300, -300, 0}
 }
-CONSTRUCTION_OBJECTS[5] = {
+CONSTRUCTION_OBJECTS[4] = {
     ID = 852,
     BaseRotation = {0, 0, 0},
     Scale = {0.75, 1, 0.8},
     RelativeOffset = {-300, 0, 0},
     SelfOffset = {0, 0, 0},
-    SwapLayout = true,
     GlobalOffset = {300, -300, 0}
 }
 --CONSTRUCTION_OBJECTS[index] = object id -- https://dev.playonset.com/wiki/Objects
